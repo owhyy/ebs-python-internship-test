11b29ebdc6be8e85ea162c23fcf0178a16d46fca
owhyy <ionbabin123@mail.ru>
2022-05-19 17:52:04 +0300

    2: admin page shows blog title and status

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
@@ -15,3 +16,10 @@ class Blog(models.Model):
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.enabled:
+            status = "enabled"
+        else:
+            status = "disabled"
+        return f"{self.title} {status}"
