ad35ccce402f64cd9432ec0fa94e3a27408b3b3a
owhyy <ionbabin123@mail.ru>
2022-05-19 18:26:38 +0300

    3: added blog creation endpoint

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.blog.views import CategoryViewSet, BlogListView, BlogItemView
+from apps.blog.views import CategoryViewSet, BlogListView, BlogItemView, CreateBlogView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -11,4 +11,5 @@ urlpatterns = router.urls
 urlpatterns += [
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog/<int:pk>/', BlogItemView.as_view(), name='blog_item'),
+    path('blog/create/', CreateBlogView.as_view(), name='blog_create'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
-from rest_framework.generics import GenericAPIView, get_object_or_404
+from rest_framework.generics import GenericAPIView, CreateAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from drf_util.decorators import serialize_decorator
 
 from apps.blog.models import Category, Blog
 from apps.blog.serializers import CategorySerializer, BlogSerializer
@@ -24,7 +25,7 @@ class BlogListView(GenericAPIView):
         return Response(BlogSerializer(blogs, many=True).data)
 
 
-class BlogItemView(GenericAPIView):
+class BlogItemView(CreateAPIView):
     serializer_class = BlogSerializer
 
     permission_classes = (AllowAny,)
@@ -32,5 +33,26 @@ class BlogItemView(GenericAPIView):
 
     def get(self, request, pk):
         blog = get_object_or_404(Blog.objects.filter(pk=pk))
+
+        return Response(BlogSerializer(blog).data)
+
+class CreateBlogView(GenericAPIView):
+    serializer_class = BlogSerializer
+
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
+
+    @serialize_decorator(BlogSerializer)
+    def post(self, request):
+        validated_data = request.serializer.validated_data
+
+        blog = Blog.objects.create(
+            title=validated_data['title'],
+            slug=validated_data['slug'],
+            enabled=validated_data['enabled'],
+            body=validated_data['body'],
+            category=validated_data['category'],
+        )
+        blog.save()
 
         return Response(BlogSerializer(blog).data)
