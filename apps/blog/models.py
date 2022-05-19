6cb044c72ea382dab290b1ba389071bd630335d4
owhyy <ionbabin123@mail.ru>
2022-05-19 17:33:36 +0300

    1: added enabled boolean field

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -11,6 +11,7 @@ class Category(models.Model):
 class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
+    enabled = models.BooleanField()
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
